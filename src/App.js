99d4f7b897bdc82cc7067f9d4d0fe53282d116c1
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-11-02 19:44:56 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import {Navbar} from 'react-bootstrap'
 
 class App extends Component {   
   
-  render() {
-    return (
-      <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              // <a href="#">CatBook</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-      </div>
-    );
-  }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <Navbar>
+  //         <Navbar.Header>
+  //           <Navbar.Brand>
+  //             <a href="#">CatBook</a>
+  //           </Navbar.Brand>
+  //         </Navbar.Header>
+  //       </Navbar>
+  //     </div>
+  //   );
+  // }
 }
 
 
